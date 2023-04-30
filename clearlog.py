3e10116c9aa5c7e0f3218ef4ca098af02d2dc7eb
Andrew <drew.escorcia@gmail.comm>
2023-04-30 16:09:04 -0400

    new file:   clearlog.py 	modified:   webhooksender.py

---
+++ clearlog.py
@@ -0,0 +1,5 @@
+import csv
+FULL_CALL_LOG = "/home/pi/callerid/full_call_log.csv"
+with open(FULL_CALL_LOG, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["number", "timestamp", "name"])
